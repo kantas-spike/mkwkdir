ef0028b0dcbd06d25f1c838d05ef5b00d1bf108e
kanta <101532773+kantas-spike@users.noreply.github.com>
2023-09-22 23:19:57 +0900

    feat: 作業ディレクトリ作成用関数を追加

---
+++ test/suite/utils.test.js
@@ -0,0 +1,94 @@
+const { expect } = require("chai");
+
+const fs = require("fs");
+const path = require("path");
+
+const utils = require("../../utils");
+
+const { runTestInTempDir } = require("../test-utils");
+
+suite("utils.hasPrefix Test Suite", () => {
+  test("bad dirname", () => {
+    expect(utils.hasPrefix("aaaa")).to.equal(false);
+    expect(utils.hasPrefix("001aaaa")).to.equal(false);
+    expect(utils.hasPrefix("001_")).to.equal(false);
+  });
+  test("good dirname", () => {
+    expect(utils.hasPrefix("001_aaaa")).to.equal(true);
+    expect(utils.hasPrefix("000_a")).to.equal(true);
+  });
+});
+
+suite("utils.formatDirName", () => {
+  test("falsy dirname", () => {
+    expect(() => utils.formatDirName("", 1)).to.throw(/invalid dirname/);
+  });
+
+  test("format dirname", () => {
+    expect(utils.formatDirName("aaa", 1)).to.equal("001_aaa");
+  });
+});
+
+suite("utils.getMaxPrefixNo Test Suite", () => {
+  test("dosen't exists dir", async () => {
+    try {
+      await utils.getMaxPrefixNo("/notexistsDirPath");
+    } catch (error) {
+      expect(error).to.match(/not exists/);
+    }
+  });
+
+  test("is not dir", async () => {
+    await runTestInTempDir(async (tmpdir) => {
+      const wkFile = path.join(tmpdir, "test.txt");
+      fs.writeFileSync(wkFile, "test");
+      // console.log(wkFile, fs.existsSync(wkFile));
+      try {
+        await utils.getMaxPrefixNo(wkFile);
+      } catch (error) {
+        expect(error).to.match(/not directory/);
+      }
+    });
+  });
+
+  test("empty dir", async () => {
+    await runTestInTempDir(async (tmpdir) => {
+      // console.log(tmpdir, fs.existsSync(tmpdir));
+      const no = await utils.getMaxPrefixNo(tmpdir);
+      expect(no).to.equal(0);
+    });
+  });
+
+  test("dir with subdirs", async () => {
+    await runTestInTempDir(async (tmpdir) => {
+      // console.log(tmpdir, fs.existsSync(tmpdir));
+      const sub_dirs = ["001_aaa", "005_bbb", "009_ccc"];
+      for (const subdir of sub_dirs) {
+        fs.mkdirSync(path.join(tmpdir, subdir));
+      }
+      const no = await utils.getMaxPrefixNo(tmpdir);
+      expect(no).to.equal(9);
+    });
+  });
+});
+
+suite("utils.makeWkDir", async () => {
+  test("make work dir", async () => {
+    await runTestInTempDir(async (tmpdir) => {
+      let no = await utils.getMaxPrefixNo(tmpdir)
+      expect(no).to.equal(0)
+      expect(fs.existsSync(path.join(tmpdir, "001_aaa"))).to.equal(false)
+      await utils.makeWkDir(tmpdir, "aaa")
+      expect(fs.existsSync(path.join(tmpdir, "001_aaa"))).to.equal(true)
+
+      no = await utils.getMaxPrefixNo(tmpdir)
+      expect(no).to.equal(1)
+      expect(fs.existsSync(path.join(tmpdir, "002_bbb"))).to.equal(false)
+      await utils.makeWkDir(tmpdir, "bbb")
+      expect(fs.existsSync(path.join(tmpdir, "002_bbb"))).to.equal(true)
+
+      no = await utils.getMaxPrefixNo(tmpdir)
+      expect(no).to.equal(2)
+    })
+  })
+})
---
+++ utils.js
@@ -0,0 +1,60 @@
+const path = require("path");
+const fs = require("fs/promises");
+
+const getMaxPrefixNo = async (dir) => {
+  const absPath = path.resolve(dir);
+
+  let stat;
+  try {
+    stat = await fs.lstat(dir);
+  } catch (error) {
+    throw new Error(`dir not exists: ${absPath}`);
+  }
+
+  if (!stat.isDirectory()) {
+    throw new Error(`${absPath} is not directory`);
+  }
+
+  let maxPrefixNo = 0;
+  // サブディレクトリの一覧を取得
+  const entries = await fs.readdir(absPath, { withFileTypes: true });
+  // サブディレクトリ名からプレフィックス取得
+  for (const entry of entries) {
+    if (entry.isDirectory() && hasPrefix(entry.name)) {
+      const prefixNo = Number(entry.name.match(/(\d+)_.+/)[1])
+      if (prefixNo > maxPrefixNo) {
+        maxPrefixNo = prefixNo
+      }
+    }
+  }
+  return maxPrefixNo;
+};
+
+const hasPrefix = (dirName) => {
+  if (dirName.match(/\d+_.+/)) {
+    return true
+  } else {
+    return false
+  }
+};
+
+const formatDirName = (dirName, prefixNo) => {
+  if (!dirName) {
+    throw new Error(`invalid dirname: ${dirName}`)
+  }
+  return `${String(prefixNo).padStart(3, '0')}_${dirName}`
+};
+
+const makeWkDir = async (baseDir, wkDirName) => {
+  const no = await getMaxPrefixNo(baseDir)
+  const wkNameWithNo = formatDirName(wkDirName, no+1)
+  const absPath = path.join(baseDir, wkNameWithNo)
+  await fs.mkdir(absPath, { recursive: true})
+};
+
+module.exports = {
+  getMaxPrefixNo,
+  formatDirName,
+  makeWkDir,
+  hasPrefix,
+};
