ddad48b2cf26a08e7154e5ae5128272a3d91402c
kanta <101532773+kantas-spike@users.noreply.github.com>
2023-09-24 01:40:49 +0900

    feat: 作業ディレクトリ作成コマンドを追加

--- extension.js
+++ extension.js
@@ -1,6 +1,56 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
+const vscode = require("vscode");
+const utils = require("./utils");
+const child_process = require("child_process");
+const fs = require("fs/promises");
+
+function makeWkDir(name, baseDirKey) {
+  return async () => {
+    try {
+      const dirName = await vscode.window.showInputBox({
+        title: `${name}用の作業ディレクトリ名入力:`,
+        validateInput: (input) => {
+          if (input.includes(" ")) {
+            return "名前に空白は使用できません";
+          } else if (input.match(/[\\¥\/:*?"<>|]/g)) {
+            return '名前に次の文字は使用できません: \\ ¥ / : * ? " < > |';
+          } else {
+            return null;
+          }
+        },
+      });
+
+      if (!dirName) {
+        vscode.window.showErrorMessage("作業ディレクトリ名を選択してください");
+        return;
+      }
+
+      const baseDir = vscode.workspace
+        .getConfiguration("make-wkdir")
+        .get(baseDirKey);
+      try {
+        await fs.stat(baseDir)
+      } catch (error) {
+        vscode.window.showInformationMessage(`${baseDir}が存在しないため作成しました`)
+        await fs.mkdir(baseDir, { recursive: true })
+      }
+
+      const wkDirPath = await utils.makeWkDir(baseDir, dirName);
+      vscode.window.showInformationMessage(
+        `作業ディレクトリを作成しました: ${wkDirPath}`
+      );
+      child_process.spawn(`code "${wkDirPath}"`, {
+        shell: true,
+        detached: true,
+      });
+    } catch (err) {
+      vscode.window.showErrorMessage(
+        `予期しないエラーが発生しました。: ${err.message}`
+      );
+    }
+  };
+}
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -9,28 +59,40 @@ const vscode = require('vscode');
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log('Congratulations, your extension "make-wkdir" is now active!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "make-wkdir" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('make-wkdir.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from make-wkdir!');
-	});
-
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      "make-wkdir.makeWkDirForProducts",
+      makeWkDir("Products", "productsPath")
+    )
+  );
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      "make-wkdir.makeWkDirForTools",
+      makeWkDir("Tools", "toolsPath")
+    )
+  );
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      "make-wkdir.makeWkDirForLearning",
+      makeWkDir("Tools", "learningPath")
+    )
+  );
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      "make-wkdir.makeWkDirForSpike",
+      makeWkDir("Tools", "spikePath")
+    )
+  );
 }
 
 // This method is called when your extension is deactivated
 function deactivate() {}
 
 module.exports = {
-	activate,
-	deactivate
-}
+  activate,
+  deactivate,
+};
