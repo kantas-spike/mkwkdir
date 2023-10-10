e6bae76226f943c5e0bd965e8f6a66968f2b6a01
kanta <101532773+kantas-spike@users.noreply.github.com>
2023-10-10 10:05:05 +0900

    test: テスト用ワークスペースでのテストを追加

---
+++ test_with_folder/suite/configuration.test.js
@@ -0,0 +1,44 @@
+const { expect } = require("chai");
+const utils = require("../../utils")
+const os = require('os')
+const path = require('path')
+
+// You can import and use all API from the 'vscode' module
+// as well as import your extension to test it
+const vscode = require("vscode");
+const { prepareWorkspace } = require("../../fixtures/fixture-utils");
+// const myExtension = require('../extension');
+
+const FIXTURE_DIR = path.join(__dirname, "../../fixtures");
+const WKDIR_ROOT_DIR = path.join(FIXTURE_DIR, "wkdir_root");
+const WORKSPACE_DIR = path.join(FIXTURE_DIR, "workspace")
+
+suite("Configuration Test Suite", () => {
+  // vscode.window.showInformationMessage("Start all tests of configuration.");
+  suiteSetup("workspaceを初期化", () => {
+    prepareWorkspace(WORKSPACE_DIR, WKDIR_ROOT_DIR)
+  })
+
+  test("get workspace's configuration value", () => {
+    const settings = vscode.workspace.getConfiguration("mkwkdir");
+
+    expect(settings.get("productsPath")).to.equal(
+      `${WKDIR_ROOT_DIR}/hacking/products`
+    );
+    expect(utils.getBaseDirPath("productsPath")).to.equal(`${WKDIR_ROOT_DIR}/hacking/products`)
+
+    expect(settings.get("toolsPath")).to.equal(`${WKDIR_ROOT_DIR}/hacking/tools`);
+    expect(utils.getBaseDirPath("toolsPath")).to.equal(`${WKDIR_ROOT_DIR}/hacking/tools`)
+
+    expect(settings.get("learningPath")).to.equal(
+      `${WKDIR_ROOT_DIR}/hacking/learning`
+    );
+    expect(utils.getBaseDirPath("learningPath")).to.equal(`${WKDIR_ROOT_DIR}/hacking/learning`)
+
+    expect(settings.get("spikePath")).to.equal(`${WKDIR_ROOT_DIR}/hacking/spike`);
+    expect(utils.getBaseDirPath("spikePath")).to.equal(`${WKDIR_ROOT_DIR}/hacking/spike`)
+
+    expect(settings.get("resourcesPath")).to.equal(`${WKDIR_ROOT_DIR}/hacking/res`);
+    expect(utils.getBaseDirPath("resourcesPath")).to.equal(`${WKDIR_ROOT_DIR}/hacking/res`)
+  });
+});
