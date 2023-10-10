100052d25b60384b2d8543f63f28739579b33a17
kanta <101532773+kantas-spike@users.noreply.github.com>
2023-10-11 03:29:03 +0900

    style: ログのインデントを微調整

--- fixtures/fixture-utils.js
+++ fixtures/fixture-utils.js
@@ -3,20 +3,20 @@ const path = require("path")
 
 function prepareWkdirRoot(wkRoot, dirPathToCopy) {
   if (fs.existsSync(wkRoot)) {
-    console.log(`${wkRoot}を削除...`);
+    console.log(`      ${wkRoot}を削除...`);
     fs.rmSync(wkRoot, { recursive: true, force: true });
   }
-  console.log(`${dirPathToCopy}を${wkRoot}としてcopy...`);
+  console.log(`      ${dirPathToCopy}を${wkRoot}としてcopy...`);
   fs.cpSync(dirPathToCopy, wkRoot, { recursive: true });
 }
 
 function prepareWorkspace(workspaceDir, wkRoot) {
   const settingsPath = path.join(workspaceDir, ".vscode/settings.json")
   if (fs.existsSync(settingsPath)) {
-    console.log(`${settingsPath}を削除...`)
+    console.log(`      ${settingsPath}を削除...`)
     fs.rmSync(settingsPath)
   }
-  console.log(`${settingsPath}を生成...`)
+  console.log(`      ${settingsPath}を生成...`)
   const settings = getSettings(wkRoot)
   fs.writeFileSync(settingsPath, JSON.stringify(settings))
 }
