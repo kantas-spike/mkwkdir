d9e5fda24657d74cea401950f4d43cf050ad8a99
kanta <101532773+kantas-spike@users.noreply.github.com>
2023-10-10 10:02:10 +0900

    test: テスト用データ・環境を準備

---
+++ fixtures/fixture-utils.js
@@ -0,0 +1,37 @@
+const fs = require("fs");
+const path = require("path")
+
+function prepareWkdirRoot(wkRoot, dirPathToCopy) {
+  if (fs.existsSync(wkRoot)) {
+    console.log(`${wkRoot}を削除...`);
+    fs.rmSync(wkRoot, { recursive: true, force: true });
+  }
+  console.log(`${dirPathToCopy}を${wkRoot}としてcopy...`);
+  fs.cpSync(dirPathToCopy, wkRoot, { recursive: true });
+}
+
+function prepareWorkspace(workspaceDir, wkRoot) {
+  const settingsPath = path.join(workspaceDir, ".vscode/settings.json")
+  if (fs.existsSync(settingsPath)) {
+    console.log(`${settingsPath}を削除...`)
+    fs.rmSync(settingsPath)
+  }
+  console.log(`${settingsPath}を生成...`)
+  const settings = getSettings(wkRoot)
+  fs.writeFileSync(settingsPath, JSON.stringify(settings))
+}
+
+function getSettings(wkRoot) {
+  return {
+    "mkwkdir.learningPath": `${wkRoot}/hacking/learning`,
+    "mkwkdir.productsPath": `${wkRoot}/hacking/products`,
+    "mkwkdir.resourcesPath": `${wkRoot}/hacking/res`,
+    "mkwkdir.spikePath": `${wkRoot}/hacking/spike`,
+    "mkwkdir.toolsPath": `${wkRoot}/hacking/tools`,
+  }
+}
+
+module.exports = {
+  prepareWkdirRoot,
+  prepareWorkspace,
+}
